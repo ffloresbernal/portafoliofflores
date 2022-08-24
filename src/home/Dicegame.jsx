b9a612a0480a9a06e71d4031b6371d5e7619b643
ffloresbernal <101959375+ffloresbernal@users.noreply.github.com>
2022-08-24 15:43:27 -0400

    agregado por segunda vez dicegame4

--- src/home/Dicegame.jsx
+++ src/home/Dicegame.jsx
@@ -4,11 +4,11 @@
  import { Jueguito, MobileIcon,
          Divcpu,Avatar,TableCPU,TablePlayer,
          Tablecell,EspacioDado, DivPlayer } from './Home.component';
- //import { GiRollingDices } from "react-icons/gi";
+ import { GiRollingDices } from "react-icons/gi";
 
  export const Dicegame = () => {
    const [popup, setpopup] = useState(false);
-   //const [dadoCPU, setDadoCPU] = useState(1);
+   const [dadoCPU, setDadoCPU] = useState(1);
 
   //  function randomNumero(min, max) {
   //    return (Math.floor(Math.random() * (max - min + 1)) + min)
@@ -16,124 +16,103 @@
 
 
    return ( 
-   <div>
-    <IconContext.Provider value={{style:{ fontSize:"2rem"} }} >
-      <MobileIcon onClick={()=> setpopup(!popup)}>
-        {popup ? <FaTimes /> : <FaDice />  }
-      </MobileIcon>
-      <br></br>
-      <Jueguito open={popup}>
+
+    <div>      
+      {/* juego de los daditos */}
+      <IconContext.Provider value={{style:{ fontSize:"2rem"} }} >
         <MobileIcon onClick={()=> setpopup(!popup)}>
           {popup ? <FaTimes /> : <FaDice />  }
         </MobileIcon>
-        <Divcpu>
+        <br></br>
 
-        </Divcpu>
-        <DivPlayer>
+        <Jueguito open={popup}>
+        <MobileIcon onClick={()=> setpopup(!popup)}>
+          {popup ? <FaTimes /> : <FaDice />  }
+        </MobileIcon>
+          <h1> Dice Game </h1>
           
-        </DivPlayer>
-      </Jueguito>
+          <Divcpu>
+            <Avatar>
+              <h1>AVATAR CPU</h1>
+            </Avatar>
+            <TableCPU>
+                <theader>{/* sumas de puntaje */}
+                  <tr>
+                    <th colSpan={3}>CPU</th>
+                  </tr>
+                  <tr>
+                    <th id='suma1'>1</th>{/* es la suma de 0.0+ 0.1 + 0.2 */}
+                    <th id='suma2'>2</th>{/* es la suma de 1.0+ 1.1 + 1.2 */}
+                    <th id='suma3'>3</th>{/* es la suma de 2.0+ 2.1 + 2.2 */}
+                  </tr>
+                </theader>
+                <tbody>
+                  <tr>
+                    <Tablecell id='arregloCPU [0,0]'>1</Tablecell>
+                    <Tablecell id='arregloCPU [1,0]'>2</Tablecell>
+                    <Tablecell id='arregloCPU [2,0]'>3</Tablecell>
+                  </tr>
+                  <tr>
+                    <Tablecell id='arregloCPU [0,1]'>1</Tablecell>
+                    <Tablecell id='arregloCPU [1,1]'>2</Tablecell>
+                    <Tablecell id='arregloCPU [2,1]'>3</Tablecell>
+                  </tr>
+                  <tr>
+                    <Tablecell id='arregloCPU [0,2]'>1</Tablecell>
+                    <Tablecell id='arregloCPU [1,2]'>2</Tablecell>
+                    <Tablecell id='arregloCPU [2,2]'>3</Tablecell>
+                  </tr>
+                </tbody>
+            </TableCPU>
+            <EspacioDado>
+              <h5>Resultado : {dadoCPU.toString()}</h5>
+              {/* <GiRollingDices onClick={()=>setDadoCPU(randomNumero(1,6))} /> */}
+            </EspacioDado>
+          </Divcpu>
 
-    </IconContext.Provider>
-   </div>)
-
-
-//     <div>      
-//       {/* juego de los daditos */}
-//       <IconContext.Provider value={{style:{ fontSize:"2rem"} }} >
-//         <MobileIcon onClick={()=> setpopup(!popup)}>
-//           {popup ? <FaTimes /> : <FaDice />  }
-//         </MobileIcon>
-//         <br></br>
-
-//         <Jueguito open={popup}>
-//         <MobileIcon onClick={()=> setpopup(!popup)}>
-//           {popup ? <FaTimes /> : <FaDice />  }
-//         </MobileIcon>
-//           <h1> Dice Game </h1>
-          
-//           <Divcpu>
-//             <Avatar>
-//               <h1>AVATAR CPU</h1>
-//             </Avatar>
-//             <TableCPU>
-//                 <theader>{/* sumas de puntaje */}
-//                   <tr>
-//                     <th colSpan={3}>CPU</th>
-//                   </tr>
-//                   <tr>
-//                     <th id='suma1'>1</th>{/* es la suma de 0.0+ 0.1 + 0.2 */}
-//                     <th id='suma2'>2</th>{/* es la suma de 1.0+ 1.1 + 1.2 */}
-//                     <th id='suma3'>3</th>{/* es la suma de 2.0+ 2.1 + 2.2 */}
-//                   </tr>
-//                 </theader>
-//                 <tbody>
-//                   <tr>
-//                     <Tablecell id='arregloCPU [0,0]'>1</Tablecell>
-//                     <Tablecell id='arregloCPU [1,0]'>2</Tablecell>
-//                     <Tablecell id='arregloCPU [2,0]'>3</Tablecell>
-//                   </tr>
-//                   <tr>
-//                     <Tablecell id='arregloCPU [0,1]'>1</Tablecell>
-//                     <Tablecell id='arregloCPU [1,1]'>2</Tablecell>
-//                     <Tablecell id='arregloCPU [2,1]'>3</Tablecell>
-//                   </tr>
-//                   <tr>
-//                     <Tablecell id='arregloCPU [0,2]'>1</Tablecell>
-//                     <Tablecell id='arregloCPU [1,2]'>2</Tablecell>
-//                     <Tablecell id='arregloCPU [2,2]'>3</Tablecell>
-//                   </tr>
-//                 </tbody>
-//             </TableCPU>
-//             <EspacioDado>
-//               <h5>Resultado : {dadoCPU.toString()}</h5>
-//               <GiRollingDices onClick={()=>setDadoCPU(randomNumero(1,6))} />
-//             </EspacioDado>
-//           </Divcpu>
-
-//           <DivPlayer>
+          <DivPlayer>
             
-//             <EspacioDado>
-//               <h5>Resultado : </h5>
-//             </EspacioDado>
-//             <TablePlayer>
-//             <theader>{/* sumas de puntaje */}
-//                   <tr>
-//                     <th colSpan={3}>CPU</th>
-//                   </tr>
-//                   <tr>
-//                     <th id='suma1'>1</th>{/* es la suma de 0.0+ 0.1 + 0.2 */}
-//                     <th id='suma2'>2</th>{/* es la suma de 1.0+ 1.1 + 1.2 */}
-//                     <th id='suma3'>3</th>{/* es la suma de 2.0+ 2.1 + 2.2 */}
-//                   </tr>
-//                 </theader>
-//                 <tbody>
-//                   <tr>
-//                     <Tablecell id='arregloCPU [0,0]'>1</Tablecell>
-//                     <Tablecell id='arregloCPU [1,0]'>2</Tablecell>
-//                     <Tablecell id='arregloCPU [2,0]'>3</Tablecell>
-//                   </tr>
-//                   <tr>
-//                     <Tablecell id='arregloCPU [0,1]'>1</Tablecell>
-//                     <Tablecell id='arregloCPU [1,1]'>2</Tablecell>
-//                     <Tablecell id='arregloCPU [2,1]'>3</Tablecell>
-//                   </tr>
-//                   <tr>
-//                     <Tablecell id='arregloCPU [0,2]'>1</Tablecell>
-//                     <Tablecell id='arregloCPU [1,2]'>2</Tablecell>
-//                     <Tablecell id='arregloCPU [2,2]'>3</Tablecell>
-//                   </tr>
-//                 </tbody>
-//             </TablePlayer>
-//             <Avatar>
-//               <h1>AVATAR PLAYER</h1>
-//             </Avatar>
-//           </DivPlayer>
-//         </ Jueguito>
+            <EspacioDado>
+              <h5>Resultado : </h5>
+            </EspacioDado>
+            <TablePlayer>
+            <theader>{/* sumas de puntaje */}
+                  <tr>
+                    <th colSpan={3}>CPU</th>
+                  </tr>
+                  <tr>
+                    <th id='suma1'>1</th>{/* es la suma de 0.0+ 0.1 + 0.2 */}
+                    <th id='suma2'>2</th>{/* es la suma de 1.0+ 1.1 + 1.2 */}
+                    <th id='suma3'>3</th>{/* es la suma de 2.0+ 2.1 + 2.2 */}
+                  </tr>
+                </theader>
+                <tbody>
+                  <tr>
+                    <Tablecell id='arregloCPU [0,0]'>1</Tablecell>
+                    <Tablecell id='arregloCPU [1,0]'>2</Tablecell>
+                    <Tablecell id='arregloCPU [2,0]'>3</Tablecell>
+                  </tr>
+                  <tr>
+                    <Tablecell id='arregloCPU [0,1]'>1</Tablecell>
+                    <Tablecell id='arregloCPU [1,1]'>2</Tablecell>
+                    <Tablecell id='arregloCPU [2,1]'>3</Tablecell>
+                  </tr>
+                  <tr>
+                    <Tablecell id='arregloCPU [0,2]'>1</Tablecell>
+                    <Tablecell id='arregloCPU [1,2]'>2</Tablecell>
+                    <Tablecell id='arregloCPU [2,2]'>3</Tablecell>
+                  </tr>
+                </tbody>
+            </TablePlayer>
+            <Avatar>
+              <h1>AVATAR PLAYER</h1>
+            </Avatar>
+          </DivPlayer>
+        </ Jueguito>
         
 
-//       </IconContext.Provider>
+      </IconContext.Provider>
       
-//     </div>
-//   )
-// }
+    </div>
+  )
+}
